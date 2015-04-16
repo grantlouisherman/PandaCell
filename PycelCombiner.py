053ed8ea72795406a23c7e498611bacd748513a4
gr <grantlouisherman041@gmail.com>
2015-04-16 17:06:21 -0400

    version 1.1
    This is the start of a function that will take in user input and write
    to file

--- PycelCombiner.py
+++ PycelCombiner.py
@@ -1,8 +1,8 @@
 import csv
 import pandas as pd
+#@author: Grant Herman
 
-
-
+#This will combine two csv files into on output file
 def twoCSVCombiner(fileONE,fileTWO,outPath):
     
     fileONE = pd.read_csv(fileONE,sep=',')
@@ -13,6 +13,22 @@ def twoCSVCombiner(fileONE,fileTWO,outPath):
     
     fileOneDataFrame.to_csv(outPath,sep=',',header=True)
     fileTwoDataFrame.to_csv(outPath,sep=',',header=True)
+
+#This is still a work in progress
+def CSVCombinerInput(fileIn,outPath):
+    fileIn = pd.read_csv(fileIn, sep=',')
+        outPath = open(outPath,'wb')
+        fileDataFrame = pd.DataFrame(fileIn)
+        fileDataFrame.to_csv(outPath,sep=',',header=True)
+        
+        more = input("Do you want to add another file?(y/n)[Note: Please put "" around your answer")
+        
+        if(more == "y"):
+            File = input("Name of file input")
+            Path = input("Name of path")
+                return CSVCombinerInput(File,Path)
+        else:
+            return
 
 
     
